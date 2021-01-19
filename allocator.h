282b802650828626f439b706431029c31591b075
Lazuli <lazuli64@protonmail.com>
2021-01-19 13:45:40 -0300

    Added include guard.

--- allocator.h
+++ allocator.h
@@ -1,6 +1,11 @@
+#ifndef ALLOCATOR_INCLUDE_GUARD
+#define ALLOCATOR_INCLUDE_GUARD
+
 #include <stdlib.h>
 
 void *m_malloc(size_t size);
 void *m_calloc(size_t nmemb, size_t size);
 void *m_realloc(void *ptr, size_t size);
 void m_free(void *ptr);
+
+#endif /* ALLOCATOR_INCLUDE_GUARD */
